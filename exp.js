670d9fa8600d54f5c14cb420d0e5d79318e0c776
oksanulik <kalko_oksanka@ukr.net>
2023-04-23 17:43:40 +0300

    11

--- exp.js
+++ exp.js
@@ -1,1 +1,19 @@
-пмит;
+console.log(77);
+
+function deliverPizza(pizzaName) {
+  return `Delivering ${pizzaName} pizza.`;
+}
+
+function makePizza(pizzaName) {
+  return `Pizza ${pizzaName} is being prepared, please wait...`;
+}
+
+// Chande code below this line
+function makeMessage(ame, callback) {
+  console.log(`Реєструємо гостя ${ame}.`);
+  return callback(ame);
+}
+
+// console.log(deliverPizza('jvgcfbm'));
+// console.log(makePizza('HYYGGG'));
+console.log(makeMessage('nmhgnnnnnnnnn', makePizza));
