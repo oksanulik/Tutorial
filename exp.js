f911b23f532a2b135732e07f8755a02f5e36490d
OksanaT <kalko_oksanka@ukr.net>
2024-10-23 21:31:33 +0300

    newfix

--- exp.js
+++ exp.js
@@ -1,19 +1,1 @@
-console.log(77);
 
-function deliverPizza(pizzaName) {
-  return `Delivering ${pizzaName} pizza.`;
-}
-
-function makePizza(pizzaName) {
-  return `Pizza ${pizzaName} is being prepared, please wait...`;
-}
-
-// Chande code below this line
-function makeMessage(ame, callback) {
-  console.log(`Реєструємо гостя ${ame}.`);
-  return callback(ame);
-}
-
-// console.log(deliverPizza('jvgcfbm'));
-// console.log(makePizza('HYYGGG'));
-console.log(makeMessage('nmhgnnnnnnnnn', makePizza));
